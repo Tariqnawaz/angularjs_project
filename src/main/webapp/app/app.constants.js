d550767e79fd0a25eb1ba3c18db7e590e90fc521
tariq <shaikh.tariqnawaz@gmail.com>
2018-06-02 22:40:01 -0700

    procfile deleted

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('bismillahApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1527178449135)
+        .constant('BUILD_TIMESTAMP', 1527308099529)
 ;
 })();
